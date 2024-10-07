3cad6ba0f054cd9af671b565e1d5a95df9e626ba
Vitaliy <vitaliyfilimonov3@gmail.com>
2024-10-07 11:36:47 +0300

    initial commit

---
+++ src/main/java/project/demo/TaskCraftrApplication.java
@@ -0,0 +1,13 @@
+package project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskCraftrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskCraftrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/demo/TaskCraftrApplicationTests.java
@@ -0,0 +1,13 @@
+package project.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskCraftrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
