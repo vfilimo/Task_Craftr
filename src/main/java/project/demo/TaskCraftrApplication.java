4cd6b12cfaa798fd6a5ec5aef480463a14744a57
Vitaliy <vitaliyfilimonov3@gmail.com>
2024-10-07 12:10:04 +0300

    checkstyle

--- src/main/java/project/demo/TaskCraftrApplication.java
+++ src/main/java/project/demo/TaskCraftrApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TaskCraftrApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TaskCraftrApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TaskCraftrApplication.class, args);
+    }
 
 }
